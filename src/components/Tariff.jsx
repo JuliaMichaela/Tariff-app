30e367a151e2a61fb8b386732951a4feeb3b594f
JuliaMichaela <juliaperez@yandex.ru>
2024-04-22 23:07:35 +0300

    created Tafiff.jsx

---
+++ src/components/Tariff.jsx
@@ -0,0 +1,20 @@
+import "../styles/tariff.css";
+
+export default function Tariff(props) {
+    const { card, setId, theme } = props;
+
+}
+
+return (
+    <div className={`card ${classCard}`}>
+        <header className={`card_header ${theme.card_header}`}>{card.name}</header>
+        <div className={`card_price ${theme.card_price}`}>
+            <p className="card_value">{card.priceTextHigh}</p>
+            <p className="card_number">{card.price}</p>
+            <p className="card_date">{card.priceTextLow}</p>
+        </div>
+        <div className="card_speed">{card.speed}</div>
+        <footer className="card_footer">{card.condition}</footer>
+    </div>
+);
+}
