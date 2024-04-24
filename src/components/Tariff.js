532acf69b176ba2b47897ec1126e81c791f8a5d0
JuliaMichaela <juliaperez@yandex.ru>
2024-04-24 23:41:51 +0300

    created tariff.js

---
+++ src/components/Tariff.js
@@ -0,0 +1,8 @@
+const tariffs =[
+    { name:"Безлимитный 300", price: "300", priceTextHigh:"руб", priceTextLow:"/мес", speed:"до 10 Мбит/сек", condition:"Объем включенного трафика не ограничен",id: 1, },
+    { name:"Безлимитный 450", price: "450", priceTextHigh:"руб", priceTextLow:"/мес", speed:"до 50 Мбит/сек", condition:"Объем включенного трафика не ограничен", id: 2,},
+    { name:"Безлимитный 550", price: "550", priceTextHigh:"руб", priceTextLow:"/мес", speed:"до 100 Мбит/сек", condition:"Объем включенного трафика не ограничен", id: 3,},
+    { name:"Безлимитный 1000", price: "1000", priceTextHigh:"руб", priceTextLow:"/мес", speed:"до 200 Мбит/сек", condition:"Объем включенного трафика не ограничен",id: 4, },
+];
+
+export default tariffs;
