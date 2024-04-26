cfec99b72f6b7243432a226766b63b9c65cb485f
JuliaMichaela <juliaperez@yandex.ru>
2024-04-26 22:28:51 +0300

    added Tariff in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,18 @@ function App() {
 
   return (
     <>
+      <div className="plans">
+        {tariffs.map((tariff, i) =>
+          <Tariff
+            key={tariff.id}
+            card={tariff}
+            isSelected={tariff.id === selectedId}
+            setId={setSelectedId}
+            theme={themes[i]}
+          />
+        )
+        }
+      </div>
 
     </>
   )
